2755f737c30ce84acf075919ff950afeac100b18
waire <waire.tega@gmail.com>
2023-11-15 14:23:58 +0100

    method overloading

--- obj.java
+++ obj.java
@@ -10,6 +10,42 @@ class calculator {
     }
 }
 
+class Computer {
+
+    public void playMusic() {
+
+        System.out.println("Playing Music");
+    }
+
+    public String getMeAPen(int cost) {
+        if (cost >= 10) {
+            return "pen";
+        }
+       
+        return "Nothing";
+    }
+}
+
+class MethodOverload {
+
+    public int add(int n1, int n2, int n3) {
+
+        return n1+n2+n3;
+    }
+
+    public int add(int n1, int n2) { // return type and parameter type must match
+       
+       
+        return n1+n2;
+    }
+
+    public double add(double n1, int n2) { // return type and parameter type must match
+       
+       
+        return n1+n2;
+    }
+}
+
 public class obj {
     public static void main(String a[]) {
 
@@ -17,9 +53,17 @@ public class obj {
         int num2 = 5;
 
         calculator calc = new calculator(); // reference // object creation
-
         int result = calc.add(num1, num2);
-        
         System.out.println(result);
+
+        Computer comp = new Computer();
+        comp.playMusic();
+        String str = comp.getMeAPen(12);
+        System.out.println(str);
+
+        MethodOverload met = new MethodOverload();
+        int add1 = met.add(num1, num2);
+
+        System.out.println(add1);
     }
 }
