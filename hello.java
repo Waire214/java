8502c9faaef7852ebc8edbd8f491f26cea433853
waire <waire.tega@gmail.com>
2023-11-15 11:48:10 +0100

    first commit

---
+++ hello.java
@@ -0,0 +1,8 @@
+class Hello {
+    public static void main(String a[]) {
+        int b = 257;
+        byte ac = (byte) b;
+        System.out.println(b);
+        System.out.println(ac);
+    }
+}
