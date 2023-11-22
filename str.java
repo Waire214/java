6b6e4396aedb174773fe1b1fd9015080665b97c3
waire <waire.tega@gmail.com>
2023-11-22 09:15:08 +0100

    stringbuffer

--- str.java
+++ str.java
@@ -17,6 +17,16 @@ public class str {
         System.out.println(newUser.hashCode());
         System.out.println(newUser.name.charAt(0));
         System.out.println(newUser.car);
+
+        StringBuffer sb = new StringBuffer("Navin");
+         System.out.println(sb.capacity());
+         System.out.println(sb.length());
+         sb.append(" sb");
+         System.out.println(sb);
+         sb.insert(7, " java");
+         System.out.println(sb);
+         sb.setLength(30);
+         System.out.println(sb);
     }
     
 }
