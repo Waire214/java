56ea015319dc52349b0361758a8720a5e7d6c6c9
waire <waire.tega@gmail.com>
2023-11-23 21:49:07 +0100

    encapsulation

---
+++ encap.java
@@ -0,0 +1,37 @@
+public class encap {
+    public static void main(String[] args) {
+        Innerencap human = new Innerencap();
+
+        human.setAge(20);
+        System.out.println( human.getAge());
+        human.setName("Jane Doe");
+        System.out.println(human.getName());
+       
+
+    }
+}
+
+/**
+ * Innerencap
+ */
+class Innerencap {
+
+    private int age;
+    private String name;
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int setAge(int a) {
+        return age = a;
+    }
+
+    public String setName(String a) {
+        return name = a;
+    }
+}
